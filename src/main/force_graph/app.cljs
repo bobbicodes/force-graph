a5e6369c6bfd8f64d19292ad55d6333f43816c3d
porkostomus <porkostomus@mail.com>
2021-11-05 16:19:27 -0700

    draw edges

--- src/main/force_graph/app.cljs
+++ src/main/force_graph/app.cljs
@@ -20,18 +20,26 @@
    "Paraechinus" ["Erinaceinae"]
    "Erinaceidae" ["Erinaceomorpha" "Erinaceinae"]})
 
+(defonce nodes
+  (r/atom (into {}
+                (for [name (keys hedgehogs)]
+                  {name {:x (rand-int 350) :y (rand-int 350)
+                         :width 80}}))))
+
+
+
 (defn label
   [name x y]
    [:text {:id name
            :x (+ 3 x) :y y :dy "0.9em"  :text-anchor "left"
            :font-size "10px"
-           :fill "white"} name])
+           :fill "yellow"} name])
 
 (defn rect
   [x y width height]
   [:rect {:x x :y y 
           :rx 5
-          :width width :height height :fill "gray"}])
+          :width width :height height :fill "purple"}])
 
 (defn width [name]
   (-> js/document
@@ -39,28 +47,45 @@
       .getBBox
       .-width))
 
-(defonce nodes
-  (r/atom (into {}
-                (for [name (keys hedgehogs)]
-                  {name {:x (rand-int 350) :y (rand-int 350)
-                         :width 80}}))))
+(:x (get @nodes "Animalia"))
+(:y (get @nodes "Animalia"))
+
+(:x (get @nodes "Chordata"))
+(:y (get @nodes "Chordata"))
+
+
+(defn draw-edges [node]
+  (into [:g]
+        (let [edges (get hedgehogs node)]
+          (for [edge edges]
+            [:line {:x1 (+ (/ (width edge) 2) (:x (get @nodes node)))
+                    :y1 (+ 6 (:y (get @nodes node)))
+                    :x2 (+ (/ (width edge) 2)(:x (get @nodes edge)))
+                    :y2 (+ 6 (:y (get @nodes edge)))
+                    :stroke "magenta"}]))))
+
 
 (defn app []
   [:div#app
    [:h1 "Force graph"]
    [:svg {:width "100%" :view-box "0 0 400 400"}
     (into [:g]
+          (map draw-edges (keys hedgehogs)))
+    (into [:g]
           (for [name (keys hedgehogs)]
             (rect (:x (get @nodes name))
-                  (:y (get @nodes name)) 
+                  (:y (get @nodes name))
                   (:width (get @nodes name)) 13)))
     (into [:g]
           (for [name (keys hedgehogs)]
-            (label name (:x (get @nodes name)) 
-                   (:y (get @nodes name)))))
-    ]])
+            (label name (:x (get @nodes name))
+                   (:y (get @nodes name)))))]])
             
 (comment
+  
+
+(edges hedgehogs)
+  @nodes
   (first (keys hedgehogs))
 
   (-> js/document
@@ -75,6 +100,7 @@
   (doseq [name (keys hedgehogs)]
     (when (< @counter 20)
       (swap! nodes assoc-in [name :width] (+ 6 (width name))))
+    
     (swap! counter inc)))
 
 (js/setInterval update! 100)
