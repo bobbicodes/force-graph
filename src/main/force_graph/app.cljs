76f1c03d02e11c0b22f2f9c6cb28f26215b4ab0d
porkostomus <porkostomus@mail.com>
2021-11-05 19:31:09 -0700

    crude animation

--- src/main/force_graph/app.cljs
+++ src/main/force_graph/app.cljs
@@ -26,7 +26,42 @@
                   {name {:x (rand-int 350) :y (rand-int 350)
                          :width 80}}))))
 
+(def repulsion 20000) ; adjust for wider/narrower spacing
+(def spring-length 100) ; base resting length of springs
+(def step-size 0.0005)
 
+(defn edge? 
+  "Returns true if n1 is connected to n2."
+  [n1 n2]
+  (contains? (set (get hedgehogs n1)) n2))
+
+(defn node-forces 
+  "Takes the names of 2 nodes as strings, and outputs
+   the force between them in the format of [forcex forcey]."
+  [n1 n2]
+  (let [i (get @nodes n1)
+        j (get @nodes n2)
+        deltax (- (:x j) (:x i))
+        deltay (- (:y j) (:y i))
+        d2 (+ (* deltax deltax) (* deltay deltay))
+      ;; Coulomb's law -- repulsion varies inversely with square of distance
+        forcex (* (/ repulsion d2) deltax)
+        forcey (* (/ repulsion d2) deltay)
+        distance (.sqrt js/Math d2)]
+  ;; spring force along edges, follows Hooke's law
+    [(+ forcex (when (edge? n1 n2) (* (- distance spring-length) deltax)))
+     (+ forcey (when (edge? n1 n2) (* (- distance spring-length) deltay)))]))
+
+(node-forces "Animalia" "Chordata")
+(node-forces "Mesechinus" "Erinaceinae")
+
+(defn node-force 
+  "Calculates the aggregated [x y] force values for a node."
+  [node]
+  (reduce (fn [[x y] [x' y']] [(+ x x') (+ y y')])
+          (remove #(js/isNaN (first %)) (map #(node-forces node %) (keys hedgehogs)))))
+
+(node-force "Animalia")
 
 (defn label
   [name x y]
@@ -64,13 +99,16 @@
                     :y2 (+ 6 (:y (get @nodes edge)))
                     :stroke "magenta"}]))))
 
+(defonce counter (r/atom 0))
+
 
 (defn app []
   [:div#app
    [:h1 "Force graph"]
    [:svg {:width "100%" :view-box "0 0 400 400"}
-    (into [:g]
-          (map draw-edges (keys hedgehogs)))
+    (when (< 2 @counter)
+      (into [:g]
+            (map draw-edges (keys hedgehogs))))
     (into [:g]
           (for [name (keys hedgehogs)]
             (rect (:x (get @nodes name))
@@ -83,8 +121,6 @@
             
 (comment
   
-
-(edges hedgehogs)
   @nodes
   (first (keys hedgehogs))
 
@@ -94,12 +130,13 @@
       .-x)
   )
 
-(defonce counter (r/atom 0))
-
 (defn update! []
   (doseq [name (keys hedgehogs)]
-    (when (< @counter 20)
-      (swap! nodes assoc-in [name :width] (+ 6 (width name))))
+    (if (< @counter 20)
+      (swap! nodes assoc-in [name :width] (+ 6 (width name)))
+      (do (swap! nodes update-in [name :x] #(+ % (* step-size (first (node-force name)))))
+          (swap! nodes update-in [name :y] #(+ % (* step-size (last (node-force name))))))
+      )
     
     (swap! counter inc)))
 
